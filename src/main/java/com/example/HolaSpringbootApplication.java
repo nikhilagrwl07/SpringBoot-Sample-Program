ffa3e8a04fe4cebbebe8c416a8995e066778ede3
Nikhil Agrawal <nikhil.agrwl07@gmail.com>
2017-03-02 20:14:07 +0530

    removing unrequired code and configs

--- src/main/java/com/example/HolaSpringbootApplication.java
+++ src/main/java/com/example/HolaSpringbootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
@@ -16,51 +17,11 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigurationProperties
 @EnableConfigServer
 public class HolaSpringbootApplication {
-
-//	@Value("${configuration.projectName}")
-//	void setProjectName(String projectName){
-//		System.out.println("setting project name: " + projectName);
-//	}
-
-//    @Value("${configuration.author}")
-//    void setAuthor(String author){
-//        System.out.println("setting author: " + author);
-//    }
-
-//	@Autowired
-//	void setEnvironment(Environment environment){
-//		System.out.println("setting environment " + environment.getProperty("configuration.projectName"));
-//	}
-
 	@Autowired
-	void setConfiguredProjectProperties(ConfigurationProjectProperties configuredProjectProperties){
-        System.out.println("configuredProjectProperties.projectName "+configuredProjectProperties.getProjectName());
-    }
-
+	void setEnvironment(Environment environment){
+		System.out.println("setting environment " + environment.getProperty("configuration.projectName"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(HolaSpringbootApplication.class, args);
 	}
-}
-
-@Component
-@ConfigurationProperties(prefix = "configuration")
-class ConfigurationProjectProperties{
-    private String projectName;
-    private String author;
-
-    public String getProjectName() {
-        return projectName;
-    }
-
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 }
